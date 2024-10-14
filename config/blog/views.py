b45f1ea8cf3591468400db244aacaf289205fdf0
Sam <sw.waithakasamuel@gmail.com>
2024-10-14 12:16:26 +0300

    handle Pagination Errors

--- config/blog/views.py
+++ config/blog/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+from django.core.paginator import PageNotAnInteger, EmptyPage, Paginator
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
@@ -7,7 +7,15 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 5)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, post, year, month, day):
